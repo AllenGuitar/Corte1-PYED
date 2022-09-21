a05694813d12ff5abf3e82cb11b65e40f85627cf
Sara Milena Sotaquira Pineda <sarita.sotaquira@gmail.com>
2022-09-20 21:24:49 -0500

    ejer

---
+++ Ejer4..js
@@ -0,0 +1,76 @@
+let animales=["Gato","Perro","Zorro","Jabali","Panda"];
+
+let pais={
+
+    "pais":"Canada",
+    "capital":"Ottawa",
+    "Kilometros":9985000000,
+    "idioma":"Ingles-y-Frances",
+    "PIB":1643000000,
+}
+
+let artista=[
+    {
+    "Nombre":"Shawn-Milke",
+    "Nacionalidad":"Estadounidense",
+    "Genero-Musical":"Post-Hardcore",
+    "Canciones":[
+        {
+            "Nombre":"Early-Mourning",
+            "Reproducciones":492779,
+        },
+        {
+            "Nombre":"The-Lover",
+            "Reproducciones":197277,
+        },
+        {
+            "Nombre":"Goodbye,-goodnight-for-good",
+            "Reproducciones":572716,
+        }
+                ],
+    },
+
+
+    {
+        "Nombre":"Yo-Ka",
+        "Nacionalidad":"Japones",
+        "Genero-Musical":"J-Rock",
+        "Canciones":[
+            {
+                "Nombre":"Shitsu-tsubasa-no-seiiki",
+                "Reproducciones":198850,
+            },
+            {
+                "Nombre":"Lost-november",
+                "Reproducciones":3300000,
+            },
+            {
+                "Nombre":"Dictator",
+                "Reproducciones":38224,
+            }
+                    ],
+    },
+
+
+    {
+            "Nombre":"Kamijo",
+            "Nacionalidad":"Japones",
+            "Genero-Musical":"J-Rock",
+            "Canciones":[
+                {
+                    "Nombre":"Masquerade",
+                    "Reproducciones":1100000,
+                },
+                {
+                    "Nombre":"Ascended-Master",
+                    "Reproducciones":1100000,
+                },
+                {
+                    "Nombre":"Serenade",
+                    "Reproducciones":389264,
+                }
+                        ],
+    }
+    ]
+
+    console.log(artista);
