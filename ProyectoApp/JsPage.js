6eaf0cc9894b1f3fe549f029a6af2e507a89804d
Allen Walker <sarita.sotaquira@gmail.com>
2022-11-07 21:18:16 -0500

    newchanges

--- ProyectoApp/JsPage.js
+++ ProyectoApp/JsPage.js
@@ -26,3 +26,39 @@ let rutas = [
         Horario:"Auditorio",
     }
 ]
+
+//accordion principal
+
+var acc = document.getElementsByClassName("btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("activa");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+let coom=document.getElementById("coom");
+let exp=document.getElementById("exp");
+let teus=document.getElementById("teus");
+let imgcamb=document.getElementById("imgcamb");
+
+coom.onclick=function(){
+ imgcamb.src="coomofu.png";
+ }
+
+ exp.onclick=function(){
+  imgcamb.src="expresodelsol.png";
+}
+
+teus.onclick=function(){
+  imgcamb.src="teusaca.png";
+}
+
+
